2ed74b34fe668def6f71f7e43265d65a9867d97c
RFozzy688 <pokerfozzy@gmail.com>
2023-06-26 11:54:40 +0300

    create parent form

---
+++ hw_2023_06_24/Parent.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace hw_2023_06_24
+{
+    partial class Parent
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(437, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Поле для ввода текста";
+            // 
+            // Parent
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(461, 105);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Parent";
+            Text = "Parent";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
---
+++ hw_2023_06_24/Parent.cs
@@ -0,0 +1,15 @@
+namespace hw_2023_06_24
+{
+    public partial class Parent : Form
+    {
+        public Parent()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
